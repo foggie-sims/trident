061abc890280fca3d0c731afea4d8df440d745ac
Britton Smith <brittonsmith@gmail.com>
2025-09-12 12:42:01 +0100

    Delint.

--- trident/light_ray.py
+++ trident/light_ray.py
@@ -38,7 +38,6 @@ from yt.utilities.physical_constants import \
 from yt.data_objects.static_output import \
     Dataset
 from yt.frontends.ytdata.data_structures import YTDataLightRayDataset
-from yt
 
 need_hint = version.parse(metadata.version("yt")) >= \
   version.parse("4.1.dev0")
